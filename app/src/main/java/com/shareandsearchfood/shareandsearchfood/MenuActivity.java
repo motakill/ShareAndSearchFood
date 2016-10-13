938b59515a362752750adb7e3acc70ab656fd3b9
DBerto <david.berto@hotmail.com>
2016-10-14 00:38:12 +0100

    registo finalizado inicio do my profile

--- app/src/main/java/com/shareandsearchfood/shareandsearchfood/MenuActivity.java
+++ app/src/main/java/com/shareandsearchfood/shareandsearchfood/MenuActivity.java
@@ -35,7 +35,6 @@ public class MenuActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,17 +79,17 @@ public class MenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_book) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_feed) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_notebook) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_how) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_eatTime) {
 
         }
 
---
+++ app/src/main/java/com/shareandsearchfood/shareandsearchfood/MyProfile.java
@@ -0,0 +1,16 @@
+package com.shareandsearchfood.shareandsearchfood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by david_000 on 13/10/2016.
+ */
+
+public class MyProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
