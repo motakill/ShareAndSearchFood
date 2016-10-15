71d4d9ad29c8a8d3f86442bec2905e908199ede0
tiagomota <wesc123>
2016-10-15 19:15:42 +0100

    muito trabalhinho aqui ja feito ahaha

---
+++ app/src/main/java/com/shareandsearchfood/shareandsearchfood/Search_places.java
@@ -0,0 +1,13 @@
+package com.shareandsearchfood.shareandsearchfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Search_places extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_places);
+    }
+}
