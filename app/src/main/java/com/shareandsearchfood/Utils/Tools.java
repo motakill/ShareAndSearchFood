5a7c4e6847f1c71c9f4d8e7598c2ff3d05a59280
DBerto <david.berto@hotmail.com>
2016-12-12 22:55:59 +0000

    login

--- app/src/main/java/com/shareandsearchfood/Utils/Tools.java
+++ app/src/main/java/com/shareandsearchfood/Utils/Tools.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.MediaController;
@@ -40,6 +41,7 @@ public class Tools extends AppCompatActivity {
 
         AlertDialog b = dialog.create();
         b.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        b.getWindow().setLayout(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         b.show();
 
         VideoView mVideoView = (VideoView)b.findViewById(R.id.videoView);
@@ -48,12 +50,11 @@ public class Tools extends AppCompatActivity {
         MediaController mediaController = new MediaController(ctx);
         mediaController.setAnchorView(controllerAnchor);
         mVideoView.setMediaController(mediaController);
-         FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         lp.gravity = Gravity.BOTTOM;
         mediaController.setLayoutParams(lp);
-
         ((ViewGroup) mediaController.getParent()).removeView(mediaController);
-
         ((FrameLayout) b.findViewById(R.id.controllerAnchor)).addView(mediaController);
 
         mVideoView.setVideoURI(Uri.parse(url));
