2faf178f64d733a81ce37835d9fa2ed1b5b03543
DBerto <david.berto@hotmail.com>
2016-12-14 20:45:41 +0000

    stars

--- app/src/main/java/com/shareandsearchfood/shareandsearchfood/Visit_person.java
+++ app/src/main/java/com/shareandsearchfood/shareandsearchfood/Visit_person.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
